d010388b988c344a32e5ba9325f387c46fb644ca
Anvith N <164768681+therealanvith@users.noreply.github.com>
2025-03-17 12:22:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,8 @@ from discord.ext import commands
 
 # --- CONFIGURATION ---
 YOUR_USER_ID = 1208069980076118016  # Your Discord User ID
-YOUR_API_KEY = 'AIzaSyDVf6PFqLxnl0ShFokVA-cGxAZtw6fcpj0'  # Replace with your actual AI API key
-API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyDVf6PFqLxnl0ShFokVA-cGxAZtw6fcpj0'
+YOUR_API_KEY = os.getenv('API_KEY')  # Replace with your actual AI API key
+API_URL = os.getenv('API_URL')
 YOUR_BOT_TOKEN = os.getenv('BOT_TOKEN')  # Replace with your actual Discord Bot Token
 
 # --- BOT SETUP ---
